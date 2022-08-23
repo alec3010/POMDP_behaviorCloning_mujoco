edeeeccb986a49ec36c53c7ec242cf90af5f59bd
Alexander Klauber <alexander.klauber.aj1@is.naist.jp>
2022-08-23 11:37:00 +0900

    both MDP and POMDPModel available

--- models.py
+++ models.py
@@ -1,24 +1,35 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-class pyTorchModel(torch.nn.Module):
-    def __init__(self, belief_dim, obs_dim, actor_hidden_dim, action_dim, ch=2):
-        super(pyTorchModel,self).__init__()
+class MDPModel(torch.nn.Module):
+    def __init__(self, policy_input_dim, actor_hidden_dim, action_dim):
+        super(MDPModel,self).__init__()
         self.policy = torch.nn.Sequential(
-            nn.Linear(belief_dim, actor_hidden_dim),
+            nn.Linear(policy_input_dim , actor_hidden_dim),
             nn.ReLU(),
             nn.Linear(actor_hidden_dim, actor_hidden_dim),
             nn.ReLU(),
             nn.Linear(actor_hidden_dim, action_dim)
         )
+        
+        
+    def forward(self,x):
+        x = self.policy(x)
+        
+        return x
+        
+
+
+class POMDPModel(MDPModel):
+    def __init__(self, belief_dim, obs_dim, actor_hidden_dim, action_dim):
+        super(POMDPModel, self).__init__(policy_input_dim=belief_dim, actor_hidden_dim=actor_hidden_dim, action_dim=action_dim)
+        
         self.policy = self.policy.double()
         self.belief_gru = torch.nn.GRUCell(obs_dim + action_dim, belief_dim)
         self.belief_gru = self.belief_gru.double()
         
     def forward(self, memory):
-    
-            
+               
         x = torch.cat((memory['curr_ob'], memory['prev_ac']), dim=0)
         prev_belief = memory['prev_belief'].double()
         belief = self.belief_gru(x.double(), prev_belief.detach())
@@ -26,8 +37,9 @@ class pyTorchModel(torch.nn.Module):
         pol_in = belief.squeeze()
         acs = self.policy(pol_in)
         return acs, belief
-    
-        
+
+
+
             
         
 
